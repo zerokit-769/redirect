713765ff305aa588d9a9909e5f2a08ab5f831976
zerokit-769 <haidliralimutan@gmail.com>
2025-10-08 06:36:48 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'blackshadow.dpdns.org'
+      url.hostname = 'sg-do.jbrs.cloud'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
